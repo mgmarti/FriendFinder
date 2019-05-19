7f05dea8731acaea7022710dcc5ee296d87d74b0
monica <mogimartinez@gmail.com>
2019-05-19 14:59:55 -0500

    added route to home.html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,5 @@
+// Routes
+// =============================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
--- server.js
+++ server.js
@@ -10,11 +10,13 @@ var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 // =============================================================
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
