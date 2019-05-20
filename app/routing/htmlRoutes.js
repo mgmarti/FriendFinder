61dccfbb05274fa700628183e26dcfe71ea92f91
monica <mogimartinez@gmail.com>
2019-05-20 15:10:25 -0500

    updated htmlRoutes to send survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app){
     });
     
     app.get("/survey", function (req, res) {
-        res.send("This is survey")
-        // res.sendFile(path.join(__dirname, "survey.html"));
+        // res.send("This is survey")
+        res.sendFile(path.join(__dirname, "./../public/survey.html"));
     });
 }
