bb72579b96224ada747d8355874efb5d14924a30
monica <mogimartinez@gmail.com>
2019-05-19 20:12:13 -0500

    updated code to display home.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,16 @@
-// var express = require("express");
-
 var app = require("./../../server");
+var path = require("path");
 
 // Routes
 // =============================================================
 app.get("/", function (req, res) {
-    res.send("Welcome to Friend Finder!")
-    // res.sendFile(path.join(__dirname, "home.html"));
+    // res.send("Welcome to Friend Finder!")
+    res.sendFile(path.join(__dirname, "./../public/home.html"));
 });
+
+app.get("/survey", function (req, res) {
+    res.send("This is survey")
+    // res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+// console.log('I can read this')
