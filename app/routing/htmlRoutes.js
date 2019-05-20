cef40821d1bccca4781a916c5ce7e0b1ae87ad2e
monica <mogimartinez@gmail.com>
2019-05-19 20:32:52 -0500

    required htmlRoutes in server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,16 @@
-var app = require("./../../server");
 var path = require("path");
 
 // Routes
 // =============================================================
-app.get("/", function (req, res) {
-    // res.send("Welcome to Friend Finder!")
-    res.sendFile(path.join(__dirname, "./../public/home.html"));
-});
 
-app.get("/survey", function (req, res) {
-    res.send("This is survey")
-    // res.sendFile(path.join(__dirname, "survey.html"));
-});
-
-// console.log('I can read this')
+module.exports = function(app){
+    app.get("/", function (req, res) {
+        // res.send("Welcome to Friend Finder!")
+        res.sendFile(path.join(__dirname, "./../public/home.html"));
+    });
+    
+    app.get("/survey", function (req, res) {
+        res.send("This is survey")
+        // res.sendFile(path.join(__dirname, "survey.html"));
+    });
+}
--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-//Exporting express requirement
+//Routes
 // =============================================================
-module.exports = app;
+require("./app/routing/htmlRoutes")(app);
