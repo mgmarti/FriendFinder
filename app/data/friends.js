0bf88b8ee76f6ebcf394f1effe182c291c764075
monica <mogimartinez@gmail.com>
2019-05-22 16:59:49 -0500

    updated variables and for loops

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
 
 const friends = [{
-        name: "Aria",
-        photo: "https://cdn.pixabay.com/photo/2013/02/10/00/02/bear-79838__340.jpg",
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        name: "Kat",
-        photo: "https://cdn.pixabay.com/photo/2018/04/20/17/18/cat-3336579__340.jpg",
-        scores: [
-            3,
-            1,
-            1,
-            2,
-            5,
-            1,
-            2,
-            3,
-            3,
-            2
-        ]
-    }
+    name: "Aria",
+    photo: "https://cdn.pixabay.com/photo/2013/02/10/00/02/bear-79838__340.jpg",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+},
+{
+    name: "Kat",
+    photo: "https://cdn.pixabay.com/photo/2018/04/20/17/18/cat-3336579__340.jpg",
+    scores: [
+        "3",
+        "1",
+        "1",
+        "2",
+        "5",
+        "1",
+        "2",
+        "3",
+        "3",
+        "2"
+    ]
+}
 ]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,25 +10,26 @@ module.exports = function (app) {
     //Create new friends 
     // =============================================================
     app.post("/api/friends", function (req, res) {
-        let newFriend = req.body.scores;
-        let scores = [];
-        //Tracks difference between user answer
-        let totalDifference = 0;
+         let newFriendData = req.body.scores;
        
+        //Tracks difference between user answer
+        let totalDifference;
+
         //Loops through current friends
         for (let i = 0; i < friends.length; i++) {
 
             //Loops through scores
-            for (let j = 0; j < newFriend.length; j++) {
-                
+            for (let j = 0; j < friends[i].scores[j]; j++) {
+
             }
+
         }
-//Math.abs() to prevent negative values
-Math.abs()
+        //Math.abs() to prevent negative values 
+        Math.abs()
 
         //Sends user info to friends data array
-        friends.push(newFriend);
-        res.json(newFriend);
+        friends.push(newFriendData);
+        res.json(newFriendData);
     });
 
 }
