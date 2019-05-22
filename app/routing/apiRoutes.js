c49cfff90767e75358f17feba11b3a48ef72775a
monica <mogimartinez@gmail.com>
2019-05-22 12:12:03 -0500

    added start post function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,6 +12,7 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         var newfriend = req.body;
 
+        //Sends user info to friends data array
         friends.push(newfriend);
         res.json(newfriend);
     });
