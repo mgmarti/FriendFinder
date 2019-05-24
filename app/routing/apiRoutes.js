55078ff8034b864f3a5dfa9d93713607834c8a37
monica <mogimartinez@gmail.com>
2019-05-24 14:48:17 -0500

    updated post in survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,26 +10,39 @@ module.exports = function (app) {
     //Create new friends 
     // =============================================================
     app.post("/api/friends", function (req, res) {
-         let newFriendData = req.body.scores;
-       
-        //Tracks difference between user answer
-        let totalDifference;
+        //Takes the result of the user's survey 
+        let newFriendScore = req.body.scores;
+
+        const scores = [];
+        let bestMatch = 0;
+
 
         //Loops through current friends
         for (let i = 0; i < friends.length; i++) {
+            //Tracks difference between user answer
+            let totalDifference = 0;
 
             //Loops through scores
-            for (let j = 0; j < friends[i].scores[j]; j++) {
+            for (let j = 0; j < newFriendScore.length; j++) {
 
+                //Calclulate difference and stores into totalDifference. Math.abs to prevent negative values.
+                totalDifference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScore[j])));
             }
-
+            //Adds result of totalDifference to scores
+            scores.push(totalDifference);
         }
-        //Math.abs() to prevent negative values 
-        Math.abs()
 
-        //Sends user info to friends data array
-        friends.push(newFriendData);
-        res.json(newFriendData);
+        for (let i = 0; i < scores.length; i++) {
+            if (scores[i] <= scores[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        let yourMatch = friends[bestMatch];
+        res.json(yourMatch);
+
+        friends.push(req.body);
+
     });
-
+    
 }
